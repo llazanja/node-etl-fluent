7aba0bc1851e08f719fc023284776ea923c53174
llazanja <llazanja96@gmail.com>
2020-06-23 03:41:06 +0200

    Fix function order

--- src/lib/driver/ISQLDriver.ts
+++ src/lib/driver/ISQLDriver.ts
@@ -4,14 +4,14 @@ import IDriver from './IDriver';
 export default interface ISQLDriver extends IDriver {
     config: object;
 
-    createTable(query: string): Promise<void>;
-    getStreamByQuery(query: string): Promise<Stream>;
-    executeQuery(query: string): Promise<object[]>;
     batchInsertQueryWithChunks(table: string, chunkArray: object[]): Promise<void>;
-    lookupAttribute(table: string, attributeToLookup: string, joinAttribute: string, joinValue: string): Promise<string>;
-    showTables(query: string): Promise<string[]>;
     createDateDimensionTable(table: string, dateFrom: Date, dateTo: Date): Promise<any[]>;
     createTimeDimensionTable(table: string): Promise<any[]>;
-    scdUpdate(updateQuery: string, insertQuery: string): Promise<void[]>;
+    createTable(query: string): Promise<void>;
+    executeQuery(query: string): Promise<object[]>;
+    getStreamByQuery(query: string): Promise<Stream>;
     lookupAll(lookupQuery: string): Promise<object[]>;
+    lookupAttribute(table: string, attributeToLookup: string, joinAttribute: string, joinValue: string): Promise<string>;
+    scdUpdate(updateQuery: string, insertQuery: string): Promise<void[]>;
+    showTables(query: string): Promise<string[]>;
 };
