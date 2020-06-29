b9e73a96e3107401dade26ffd779f19d4138b56f
llazanja <llazanja96@gmail.com>
2020-06-29 17:41:56 +0200

    Indentation

--- src/examples/ETLSQLToSQL/flow/2/loadDProduct.ts
+++ src/examples/ETLSQLToSQL/flow/2/loadDProduct.ts
@@ -3,6 +3,11 @@ import ETLTask from '../../../../lib/ETLTask';
 
 export default function loadDProductTable() {
     new ETLTask()
-    .fromSQLDatabase(connections.mssql, 'SELECT p.ProductID AS ProductDBID, p.ProductName, c.CategoryName AS ProductCategory FROM products p INNER JOIN categories c ON p.CategoryID = c.CategoryID')
+    .fromSQLDatabase(
+        connections.mssql,
+        `SELECT p.ProductID AS ProductDBID, p.ProductName, c.CategoryName AS ProductCategory
+        FROM products p
+        INNER JOIN categories c ON p.CategoryID = c.CategoryID`
+    )
     .toSQLDatabase(connections.mysql, 'dProduct');
 };
--- src/examples/ETLSQLToSQL/flow/4/loadCOrders.ts
+++ src/examples/ETLSQLToSQL/flow/4/loadCOrders.ts
@@ -3,14 +3,19 @@ import ETLTask from '../../../../lib/ETLTask';
 
 export default function loadCOrdersTable() {
     return new ETLTask()
-    .fromSQLDatabase(connections.mssql, `SELECT p.ProductID, e.EmployeeID, s.SupplierID, c.CustomerID, CONVERT(VARCHAR(10), o.OrderDate, 111) AS OrderDate, SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS TotalPrice
-        FROM orders o JOIN orderItems od ON o.OrderID = od.OrderID 
-        JOIN products p ON od.ProductID = p.ProductID 
-        JOIN suppliers s ON p.SupplierID = s.SupplierID 
-        JOIN customers c ON o.CustomerID = c.CustomerID 
-        JOIN employees e ON o.EmployeeID = e.EmployeeID 
-        WHERE YEAR(o.OrderDate) != '2070'
-        GROUP BY p.ProductID, e.EmployeeID, s.SupplierID, c.CustomerID, o.OrderDate`)
+    .fromSQLDatabase(
+        connections.mssql,
+        `SELECT p.ProductID, e.EmployeeID, s.SupplierID,
+            c.CustomerID, CONVERT(VARCHAR(10), o.OrderDate, 111) AS OrderDate,
+            SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS TotalPrice
+            FROM orders o JOIN orderItems od ON o.OrderID = od.OrderID 
+            JOIN products p ON od.ProductID = p.ProductID 
+            JOIN suppliers s ON p.SupplierID = s.SupplierID 
+            JOIN customers c ON o.CustomerID = c.CustomerID 
+            JOIN employees e ON o.EmployeeID = e.EmployeeID 
+            WHERE YEAR(o.OrderDate) != '2070'
+            GROUP BY p.ProductID, e.EmployeeID, s.SupplierID, c.CustomerID, o.OrderDate`
+    )
     .lookupAndReplace(connections.mysql, "dDate", "OrderDate", "DateID", "Date")
     .lookupAndReplace(connections.mysql, "dProduct", "ProductID", "ProductID", "ProductDBID")
     .lookupAndReplace(connections.mysql, "dEmployee", "EmployeeID", "EmployeeID", "EmployeeDBID")
